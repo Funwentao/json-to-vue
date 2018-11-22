a25cc90be9a0a6d1ccdc8ce5f1611fd7cf8b7bfe
fun <wentao.fang@zhenai.com>
2018-11-22 21:31:03 +0800

    fix

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="drag-content" @drag="onDrag">
+
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    onDrag(e) {
+      console.log(e)
+    }
+  }
+};
+</script>
+<style lang="sass" scoped>
+
+</style>
